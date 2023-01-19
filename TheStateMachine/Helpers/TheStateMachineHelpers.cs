b6691b8d391213df4c5b48300cb41aa4d258303f
Vinicius De Martin <demartinvinicius@gmail.com>
2023-01-19 15:25:39 -0300

    New architectures

--- TheStateMachine/Helpers/TheStateMachineHelpers.cs
+++ TheStateMachine/Helpers/TheStateMachineHelpers.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Appccelerate.StateMachine.AsyncMachine;
+using Appccelerate.StateMachine.Machine;
+using StatesAndEvents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TheStateMachine.Model;
 
-namespace TheStateMachine.Helpers
+namespace TheStateMachine.Helpers;
+
+public static class TheStateMachineHelpers
 {
-    internal class TheStateMachineHelpers
+    public static MachineSpecification GetMachineSpecification(Assembly assembly)
     {
+        var specification = new MachineSpecification();
+        specification.States = new();
+
+        var statesInAssembly = assembly.ExportedTypes.Where(type => type.BaseType == typeof(BaseState))
+            .Select(x => (BaseState)Activator.CreateInstance(x, new object[] { null, null, null }));
+        return specification;
     }
 }
--- TheStateMachine/Model/MachineSpecification.cs
+++ TheStateMachine/Model/MachineSpecification.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using StatesAndEvents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheStateMachine.Model
+namespace TheStateMachine.Model;
+
+public class MachineSpecification
 {
-    internal class MachineSpecification
-    {
-    }
+    public List<BaseState> States { get; set; } 
+    public List<IGuard<BaseState, BaseState>> IntermediaryGuards { get; set; }
+    public List<IGuard<BaseState>> FinalGuards { get; set; }
 }
